30fad3ca896ae8146a5ea1d22689c26ed7eb8cda
Yunus Emre Deligöz <ye@deligoz.me>
2020-12-06 19:03:55 +0300

    Lint

--- src/Config.php
+++ src/Config.php
@@ -16,26 +16,26 @@ final class Config
     public Tokenizer $tokenizer;
 
     private string $name;
-    private int $n = 2;
-    private int $minLenght = 2;
-    private bool $unique = false;
-    private bool $excludeOriginals = false;
+    private int $n                  = 2;
+    private int $minLenght          = 2;
+    private bool $unique            = false;
+    private bool $excludeOriginals  = false;
     private int $frequencyPrecision = 7;
 
-    private int $elementLimit = 500;
-    private int $elementFirstLimit = 500;
+    private int $elementLimit        = 500;
+    private int $elementFirstLimit   = 500;
     private int $sentenceFirst1Limit = 500;
     private int $sentenceFirst2Limit = 500;
     private int $sentenceFirst3Limit = 500;
-    private int $sentenceLast1Limit = 500;
-    private int $sentenceLast2Limit = 500;
-    private int $sentenceLast3Limit = 500;
+    private int $sentenceLast1Limit  = 500;
+    private int $sentenceLast2Limit  = 500;
+    private int $sentenceLast3Limit  = 500;
 
     // endregion
 
     public function __construct(string $name)
     {
-        $this->name = $name;
+        $this->name      = $name;
         $this->tokenizer = new Tokenizer();
     }
 
